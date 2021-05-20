06196829acaa10e9b075a0c4b068e54b60b37b57
giuroperto <giu.roperto@gmail.com>
2021-05-20 20:48:28 -0300

    creating an app to apply the things learned in the layout and navigation classes

---
+++ 5.MoodApp/app/src/main/java/com/gr/android/moodapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.gr.android.moodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.gr.android.moodapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController = this.findNavController(R.id.nav_host_fragm)
+    }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
+}
---
+++ 5.MoodApp/app/src/main/java/com/gr/android/moodapp/moodPickerFragment.kt
@@ -0,0 +1,90 @@
+package com.gr.android.moodapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.gr.android.moodapp.databinding.FragmentMoodPickerBinding
+
+class moodPickerFragment : Fragment() {
+
+    private lateinit var binding: FragmentMoodPickerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mood_picker, container, false)
+
+        moodListener()
+
+        return binding.root
+    }
+
+    private fun moodListener() {
+        val clickableMoods: List<View> = listOf(binding.angryImg, binding.smugImg, binding.uwuImg, binding.yummyImg)
+
+        for (item in clickableMoods) {
+            item.setOnClickListener {
+                changeBackground(it)
+                greyAllOut(it, clickableMoods)
+            }
+        }
+    }
+
+    private fun changeBackground(view: View) {
+        when (view.id) {
+            R.id.angry_img -> binding.moodPickerConstraintLayout.setBackgroundResource(R.color.bg_red)
+            R.id.yummy_img -> binding.moodPickerConstraintLayout.setBackgroundResource(R.color.bg_orange)
+            R.id.uwu_img -> binding.moodPickerConstraintLayout.setBackgroundResource(R.color.bg_yellow)
+            R.id.smug_img -> binding.moodPickerConstraintLayout.setBackgroundResource(R.color.bg_green)
+
+            else -> binding.moodPickerConstraintLayout.setBackgroundResource(R.color.white)
+        }
+    }
+
+    private fun greyAllOut(view: View, list: List<View>) {
+        for (item in list) {
+            if (item.id != view.id) {
+                // TODO: 20/05/2021 change other icons color to gray
+                // TODO: 20/05/2021 if someone keep clicking it should always change the current icon back to normal colors
+                item.rotation = 45.0F
+
+                binding.apply {
+                    grayFilterAngry.visibility = View.VISIBLE
+                    grayFilterUwu.visibility = View.VISIBLE
+                    grayFilterSmug.visibility = View.VISIBLE
+                    grayFilterYummy.visibility = View.VISIBLE
+                }
+
+                when (view.id) {
+                    R.id.angry_img -> binding.grayFilterAngry.visibility = View.GONE
+                    R.id.yummy_img -> binding.grayFilterYummy.visibility = View.GONE
+                    R.id.uwu_img -> binding.grayFilterUwu.visibility = View.GONE
+                    R.id.smug_img -> binding.grayFilterSmug.visibility = View.GONE
+                }
+            } else {
+                item.rotation = 0.0F
+
+                binding.apply {
+                    grayFilterAngry.visibility = View.VISIBLE
+                    grayFilterUwu.visibility = View.VISIBLE
+                    grayFilterSmug.visibility = View.VISIBLE
+                    grayFilterYummy.visibility = View.VISIBLE
+                }
+
+                when (view.id) {
+                    R.id.angry_img -> binding.grayFilterAngry.visibility = View.GONE
+                    R.id.yummy_img -> binding.grayFilterYummy.visibility = View.GONE
+                    R.id.uwu_img -> binding.grayFilterUwu.visibility = View.GONE
+                    R.id.smug_img -> binding.grayFilterSmug.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+}
