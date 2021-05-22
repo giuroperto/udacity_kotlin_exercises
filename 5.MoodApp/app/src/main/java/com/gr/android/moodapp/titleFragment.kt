4b4320f8e8944edf47fd91cb7aa5463307b1e5be
giuroperto <giu.roperto@gmail.com>
2021-05-21 23:46:13 -0300

    adjusting about fragment to show content properly

--- 5.MoodApp/app/src/main/java/com/gr/android/moodapp/titleFragment.kt
+++ 5.MoodApp/app/src/main/java/com/gr/android/moodapp/titleFragment.kt
@@ -1,14 +1,14 @@
 package com.gr.android.moodapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.gr.android.moodapp.databinding.FragmentTitleBinding
 
 class titleFragment : Fragment() {
@@ -29,7 +29,18 @@ class titleFragment : Fragment() {
                 )
             )
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 
 }
