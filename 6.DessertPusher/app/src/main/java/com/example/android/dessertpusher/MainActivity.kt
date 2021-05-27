60a5aa524227907e97f871b2c65f61d3ae44fd74
giuroperto <giu.roperto@gmail.com>
2021-05-26 21:44:28 -0300

    playing with lifecycles

--- 6.DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ 6.DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -77,6 +78,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
+        dessertTimer = DessertTimer(this.lifecycle)
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -88,6 +91,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart() {
         super.onStart()
 
+        dessertTimer.startTimer()
+
         Timber.i("onStart called")
     }
 
@@ -98,6 +103,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onPause() {
         super.onPause()
+
         Timber.i("onPause called")
     }
 
@@ -108,6 +114,9 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         super.onStop()
+
+        dessertTimer.stopTimer()
+
         Timber.i("onStop called")
     }
 
