cdc2b8c746e4c2b1fe96c05ac6ffb83886b3004e
giuroperto <giu.roperto@gmail.com>
2021-08-10 11:14:54 -0300

    adding header to grid layout

--- 9.SleepTracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ 9.SleepTracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -115,7 +115,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.submitList(it)
+                adapter.addHeaderAndSubmitList(it)
             }
         })
 
@@ -129,6 +129,15 @@ class SleepTrackerFragment : Fragment() {
         })
 
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return when(position) {
+                    0 -> 3
+                    else -> 1
+                }
+            }
+
+        }
         binding.rvSleepList.layoutManager = manager
 
         return binding.root
